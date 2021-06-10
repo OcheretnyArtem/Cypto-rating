14d7a046969878c6fca909d53c473c0c7f1e3ce3
Gizcerbes <gizcerbes@outlook.com>
2021-06-10 19:26:09 +0300

    add Link

--- app/src/main/java/by/ocheretny/cyptorating/recycler/CryptoRecycler.kt
+++ app/src/main/java/by/ocheretny/cyptorating/recycler/CryptoRecycler.kt
@@ -3,11 +3,11 @@ package com.uogames.longProject.HW8.recycler
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import by.ocheretny.cyptorating.R
 import com.squareup.picasso.Picasso
 import com.uogames.lesson23.recycler.SimpleRecyclerAdapter
 import com.uogames.lesson23.recycler.SimpleViewHolder
 import com.uogames.longProject.HW8.data.entities.currency.ListingData
-import com.uogames.longProject.R
 
 class CryptoRecycler(layout: Int, items: List<ListingData.Data>) :
     SimpleRecyclerAdapter<ListingData.Data, CryptoRecycler.CryptoViewHolder>(layout, items) {
