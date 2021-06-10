d9b4ec6fe06bf3a9f0250b388e0bf19f249a5848
Gizcerbes <gizcerbes@outlook.com>
2021-06-10 20:01:44 +0300

    add Link

--- app/src/main/java/by/ocheretny/cyptorating/MainActivity.kt
+++ app/src/main/java/by/ocheretny/cyptorating/MainActivity.kt
@@ -2,10 +2,30 @@ package by.ocheretny.cyptorating
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
+import com.uogames.longProject.HW8.NetworkingViewModel
+import com.uogames.longProject.HW8.recycler.CryptoRecycler
 
 class MainActivity : AppCompatActivity() {
+    private val networkingViewModel by lazy { ViewModelProvider(this).get(NetworkingViewModel::class.java) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_hw8)
+
+        val recycler = findViewById<RecyclerView>(R.id.hw8_crypto_recycler)
+
+
+
+
+        networkingViewModel.listingData.observe(this){
+            val adapter =
+                it.data?.let { it1 -> CryptoRecycler(R.layout.fragment_item_crypto_hw8, it1) }
+            recycler.adapter = adapter
+        }
+
+        networkingViewModel.updateListingData()
+
     }
 }
---
+++ app/src/main/java/by/ocheretny/cyptorating/data/dto/currency/LatestDataResponse.kt
@@ -0,0 +1,91 @@
+package by.ocheretny.cyptorating.data.dto.currency
+
+
+import com.google.gson.annotations.SerializedName
+
+data class LatestDataResponse(
+    @SerializedName("data")
+    val `data`: Data?,
+    @SerializedName("status")
+    val status: Status?
+) {
+    data class Data(
+        @SerializedName("1")
+        val x1: X1?
+    ) {
+        data class X1(
+            @SerializedName("circulating_supply")
+            val circulatingSupply: Number?,
+            @SerializedName("cmc_rank")
+            val cmcRank: Number?,
+            @SerializedName("date_added")
+            val dateAdded: String?,
+            @SerializedName("id")
+            val id: Number?,
+            @SerializedName("is_active")
+            val isActive: Number?,
+            @SerializedName("is_fiat")
+            val isFiat: Number?,
+            @SerializedName("is_market_cap_included_in_calc")
+            val isMarketCapIncludedInCalc: Number?,
+            @SerializedName("last_updated")
+            val lastUpdated: String?,
+            @SerializedName("max_supply")
+            val maxSupply: Number?,
+            @SerializedName("name")
+            val name: String?,
+            @SerializedName("num_market_pairs")
+            val numMarketPairs: Number?,
+            @SerializedName("platform")
+            val platform: Any?,
+            @SerializedName("quote")
+            val quote: Map<String, Quote>?,
+            @SerializedName("slug")
+            val slug: String?,
+            @SerializedName("symbol")
+            val symbol: String?,
+            @SerializedName("tags")
+            val tags: List<String?>?,
+            @SerializedName("total_supply")
+            val totalSupply: Number?
+        ) {
+            data class Quote(
+                @SerializedName("last_updated")
+                val lastUpdated: String?,
+                @SerializedName("market_cap")
+                val marketCap: Number?,
+                @SerializedName("percent_change_1h")
+                val percentChange1h: Number?,
+                @SerializedName("percent_change_24h")
+                val percentChange24h: Number?,
+                @SerializedName("percent_change_30d")
+                val percentChange30d: Number?,
+                @SerializedName("percent_change_60d")
+                val percentChange60d: Number?,
+                @SerializedName("percent_change_7d")
+                val percentChange7d: Number?,
+                @SerializedName("percent_change_90d")
+                val percentChange90d: Number?,
+                @SerializedName("price")
+                val price: Number?,
+                @SerializedName("volume_24h")
+                val volume24h: Number?
+            )
+        }
+    }
+
+    data class Status(
+        @SerializedName("credit_count")
+        val creditCount: Number?,
+        @SerializedName("elapsed")
+        val elapsed: Number?,
+        @SerializedName("error_code")
+        val errorCode: Number?,
+        @SerializedName("error_message")
+        val errorMessage: Any?,
+        @SerializedName("notice")
+        val notice: Any?,
+        @SerializedName("timestamp")
+        val timestamp: String?
+    )
+}
